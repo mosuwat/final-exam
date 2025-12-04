a667712380c09853ad5cd720f0db68f95e76f8ba
mosuwat <mosuwat50@hotmail.com>
2025-12-04 15:00:19 +0700

    Added customer and driver class

--- main.py
+++ main.py
@@ -3,4 +3,20 @@ class Person:
         self.name = name
     
     def introduce(self):
-        print(self.name)
+        print(f"Hi, I'm {self.name}")
+
+class Customer(Person):
+    def __init__(self, name, address):
+        super().__init__(self, name)
+        self.address = address
+
+    def place_order(self, item):
+        pass
+
+class Driver(Person):
+    def __init__(self, name, vehicle):
+        super().__init__(self, name)
+        self.vehicle = vehicle
+    
+    def deliver(self, order):
+        pass
